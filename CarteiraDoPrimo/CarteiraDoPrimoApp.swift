0b520f796ce16eba353d08b110167706a6238f00
Joao Gripp <corguinha@gmail.com>
2023-01-26 14:55:27 +0000

    Initial Commit

---
+++ CarteiraDoPrimo/CarteiraDoPrimoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarteiraDoPrimoApp.swift
+//  CarteiraDoPrimo
+//
+//  Created by Joao Gripp on 26/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CarteiraDoPrimoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
